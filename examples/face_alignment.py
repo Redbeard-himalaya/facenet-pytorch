cda68979647be648ebfe30477c718a008a46e8e3
redbeard <developer.bannon@gmail.com>
2024-04-28 19:51:09 +1200

    add examples/face_alignment.py
    Signed-off-by: redbeard <developer.bannon@gmail.com>

---
+++ examples/face_alignment.py
@@ -0,0 +1,24 @@
+from facenet_pytorch import MTCNN, InceptionResnetV1, alignment
+from PIL import Image, ImageDraw
+import numpy as np
+import torch
+from torchvision.transforms.functional import to_pil_image
+image_path = 'fan_images/test_images/'
+image_names = ["9_0.jpg", "9.jpg", "9_3.jpg"]
+image_save_path = 'fan_images/corpped/'
+post_process = False
+keep_all = True
+if len(image_names) > 1:
+    imgs = [np.uint8(Image.open(image_path + n).convert("RGB")) for n in image_names]
+else:
+    imgs = np.uint8(Image.open(image_path + image_names[0]).convert("RGB"))
+mtcnn = MTCNN(device='cpu', post_process=post_process, margin=10, keep_all=keep_all, align=True)
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to('cpu')
+imgs_cpd, box = mtcnn(imgs, return_box=True)
+aligned = []
+for heads in imgs_cpd:
+    if heads is not None:
+        aligned.append(heads)
+aligned = torch.cat(aligned, axis=0)
+embeddings = resnet(aligned).detach().cpu()
+[[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
