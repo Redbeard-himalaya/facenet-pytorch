e6b4afe319cf4f6e19908fa1604870bb894e6599
redbeard <developer.bannon@gmail.com>
2024-05-01 20:10:27 +1200

    fix examples/face_alignment.py
    Signed-off-by: redbeard <developer.bannon@gmail.com>

--- examples/face_alignment.py
+++ examples/face_alignment.py
@@ -9,9 +9,9 @@ image_save_path = 'fan_images/corpped/'
 post_process = False
 keep_all = True
 if len(image_names) > 1:
-    imgs = [np.uint8(Image.open(image_path + n).convert("RGB")) for n in image_names]
+    imgs = [Image.open(image_path + n).convert("RGB") for n in image_names]
 else:
-    imgs = np.uint8(Image.open(image_path + image_names[0]).convert("RGB"))
+    imgs = Image.open(image_path + image_names[0]).convert("RGB")
 mtcnn = MTCNN(device='cpu', post_process=post_process, margin=10, keep_all=keep_all, align=True)
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to('cpu')
 imgs_cpd, box = mtcnn(imgs, return_box=True)
@@ -21,4 +21,7 @@ for heads in imgs_cpd:
         aligned.append(heads)
 aligned = torch.cat(aligned, axis=0)
 embeddings = resnet(aligned).detach().cpu()
+centroid = embeddings.mean(dim=0)
+x = (embeddings - centroid).norm(dim=1).cpu()
+radius = (embeddings - centroid).norm(dim=1).max().cpu()
 [[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
